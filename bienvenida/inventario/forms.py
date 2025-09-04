9cdce3410e0485cc48a11d6d32fa08d2489273bc
BenjaminEspinoza22 <benjamin.espinoza34@inacapmail.cl>
2025-09-04 15:03:46 -0400

    parte 1 crud con django

---
+++ bienvenida/inventario/forms.py
@@ -0,0 +1,40 @@
+from django import forms
+from .models import Producto
+
+class ProductoForm(forms.ModelForm):
+    class Meta:
+        model = Producto
+        fields = ['nombre', 'descripcion', 'precio', 'stock']
+
+#inventario/views.py
+from django.shortcuts import render, get_object_or_404, redirect
+from .models import Producto
+from .forms import ProductoForm
+
+#READ (list)
+def producto_detail(request, pk):
+    productos= Producto.objetcts.all()
+    return render(request, 'inventario/producto_list.html', {'object_list': productos})
+
+#READ (list)
+def producto_detail(request,pk):
+    producto = get_object_or_404(Producto,pk=pk)
+    return render(request,'inventario/producto_detail.html',{'object' : producto})
+
+#CREATE
+def producto_create(request):
+    if request.method == 'POST':
+        form = ProductoForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('producto_list')
+        else:
+            form = ProductoForm()
+            return render(request, 'inventario/producto_form.html', {'form':form})
+
+#UPDATE
+def producto_update(request, pk):
+    producto = get_object_or_404(producto, pk=pk)
+    if request.method == 'POST':
+        form = ProductoForm(request.POST, instance=producto)
+
