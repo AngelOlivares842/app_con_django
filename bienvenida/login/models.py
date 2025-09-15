8475721f6496dd4c9ed7fe44a98482a1e0148aaa
Ángel <angel.olivares04@inacapmail.cl>
2025-09-15 14:18:54 -0300

    delete login

--- bienvenida/login/models.py
+++
@@ -1,8 +0,0 @@
-from django.db import models
-
-# Create your models here.
-class User(models.Model):
-    nombre = models.CharField(max_length=25)
-    contrasena = models.CharField(max_length=10)
-
-    
--- bienvenida/login/views.py
+++
@@ -1,8 +0,0 @@
-from django.shortcuts import render, get_object_or_404, redirect
-from .models import User
-
-# Create your views here.
-def User(request, pk):
-    users = get_object_or_404(User, pk=pk)
-    return render(request, 'inventario/producto_detail.html',
-    {'object': users})
